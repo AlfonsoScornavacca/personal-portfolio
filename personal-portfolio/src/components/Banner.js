cd3f4f6508ab05efa9b38f7b044e69a21fa6dd6d
Alfonso Scornavacca <alfonso.scornavacca@raona.com>
2024-01-07 13:45:44 -0300

    Banner component created

---
+++ personal-portfolio/src/components/Banner.js
@@ -0,0 +1,69 @@
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from '../assets/img/header-img.svg'
+import { useEffect , useState } from "react";
+import TrackVisibility from 'react-on-screen';
+
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(200 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+    const toRotate = [ "Web Developer", "Cloud Admin", "Security Admin" ];
+    const period = 300;
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    
+        setText(updatedText);
+    
+        if (isDeleting) {
+          setDelta(prevDelta => prevDelta / 2);
+        }
+    
+        if (!isDeleting && updatedText === fullText) {
+          setIsDeleting(true);
+          setIndex(prevIndex => prevIndex - 1);
+          setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+          setIsDeleting(false);
+          setLoopNum(loopNum + 1);
+          setIndex(1);
+          setDelta(250);
+        } else {
+          setIndex(prevIndex => prevIndex + 1);
+        }
+      }
+    useEffect(() => {
+      let ticker = setInterval(() => {
+        tick();
+      }, delta);
+  
+      return () => { clearInterval(ticker) };
+    }, [text])
+  
+    return(
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Wolcome to my portfolio</span>
+                        <h1>{`I'm Alfonso Scornavacca `}<br></br><span className="wrap">{text}</span></h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <button onClick={() => console.log('connect')}>Let’s Connect</button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -2,8 +2,11 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useEffect, useState } from 'react';
-import navIcon1 from "../assets/img/nav-icon1.svg"
-import row from "../assets/img/arrow1.svg"
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home')
@@ -31,22 +34,22 @@ export const NavBar = () => {
         <Navbar expand="lg" className={ scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="#home">
-              <img src={""} alt="LOGO" />
+              <img src={logo} alt="LOGO" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
               <span className='navbar-toggler-icon'></span>
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home" className={activeLink === "home" ? "active navBar-link" : "navBar-link"}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === "skills" ? "active navBar-link" : "navBar-link"}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === "projects" ? "active navBar-link" : "navBar-link"}>Projects</Nav.Link>
+              <Nav.Link href="#home" className={activeLink === "home" ? "active navbar-link" : "navbar-link"}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === "skills" ? "active navbar-link" : "navbar-link"}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === "projects" ? "active navbar-link" : "navbar-link"}>Projects</Nav.Link>
             </Nav>
             <span className='navbar-text'>
                 <div className='social-icon'>
-                    <a href='#' src={row} alt='LinkedIn'/>
-                    <a href='#' src={''} alt='Instagram'/>
-                    <a href='#' src={''} alt='Facebook'/>
+                    <a href='#' src={navIcon1} alt='LinkedIn'/>
+                    <a href='#' src={navIcon2} alt='Instagram'/>
+                    <a href='#' src={navIcon3} alt='Facebook'/>
                 </div>
                 <button className='vvd' onClick={()=>{console.log("Connect")}}> <span>Let´s connect</span></button>
             </span>
